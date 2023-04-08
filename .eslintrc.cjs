ecd80afbf7aeba33411e458f6402f16e5ef53602
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-08 19:05:07 +0200

    add state management as pinia + toggle modal authentification & register

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,14 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
   }
 }
