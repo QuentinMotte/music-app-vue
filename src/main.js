87358950c7955fec0358f0ea80c652c416c8d8f1
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-09 16:38:19 +0200

    add firebaseConfig & env variables

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validations'
+import './includes/firebase'
 
 import './assets/base.css'
 import './assets/main.css'
