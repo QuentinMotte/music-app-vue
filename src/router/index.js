a52f2c1f3aab001ed14debf6750da887f7fbce3d
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-12 13:57:03 +0200

    continue router + add guarding routes + add redirects + start manage component

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,59 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
+import useUserStore from '@/stores/user'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: Home
   },
   {
+    name: 'about',
     path: '/about',
     component: About
+  },
+  {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    component: Manage,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+
+  const store = useUserStore()
+
+  if (store.userLoggedIn) {
+    next()
+    return
+  } else {
+    next({ name: 'home' })
+  }
 })
 
 export default router
