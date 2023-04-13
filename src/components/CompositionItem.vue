274ec12ae0bda871dd4c3d6da0a1c7b8cf2c6b1f
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-13 15:41:49 +0200

    finish upload with drag&drop + error + success + dynamic progressBar + create component for list the songs and start possibilities to edit/update

---
+++ src/components/CompositionItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showEditForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modifiedName }}</h4>
+      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+        <i class="fa fa-times"></i>
+      </button>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showEditForm = !showEditForm"
+      >
+        <i class="fa fa-pencil-alt"></i>
+      </button>
+    </div>
+    <div v-show="showEditForm">
+      <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
+        <div class="mb-3">
+          <label class="inline-block mb-2">Song Title</label>
+          <vee-field
+            name="modifiedName"
+            type="text"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Song Title"
+          />
+          <ErrorMessage class="text-red-600" name="modifiedName" />
+        </div>
+        <div class="mb-3">
+          <label class="inline-block mb-2">Genre</label>
+          <vee-field
+            name="genre"
+            type="text"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Genre"
+          />
+          <ErrorMessage class="text-red-600" name="genre" />
+        </div>
+        <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          @click.prevent="showEditForm = !showEditForm"
+        >
+          Go Back
+        </button>
+      </vee-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CompositionItem',
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      showEditForm: false,
+      schema: {
+        modifiedName: 'required',
+        genre: 'required'
+      }
+    }
+  },
+  methods: {
+    edit(values) {
+      console.log(values)
+    }
+  }
+}
+</script>
