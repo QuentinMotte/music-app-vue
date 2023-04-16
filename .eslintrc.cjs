82e686c61e0686d4f129002517a231569abffe94
QuentinMotte <99801733+QuentinMotte@users.noreply.github.com>
2023-04-16 18:05:57 +0200

    delete template

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,27 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true, extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'], overrides: [{
-    files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'], extends: ['plugin:cypress/recommended']
-  }], env: {
+  root: true,
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  overrides: [
+    {
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
+    }
+  ],
+  env: {
     node: true
-  }, parserOptions: {
+  },
+  parserOptions: {
     ecmaVersion: 'latest'
-  }, rules: {
-    'vue/multi-word-component-names': 'off', 'prettier/prettier': ['error', {
-      endOfLine: 'auto'
-    }]
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
   }
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,32 @@
 <template>
   <!-- Header -->
-  <header id="header" class="bg-gray-700">
-    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+  <header id='header' class='bg-gray-700'>
+    <nav class='container mx-auto flex justify-start items-center py-5 px-4'>
       <!-- App Name -->
       <router-link
-        class="text-white font-bold uppercase text-2xl mr-4"
+        class='text-white font-bold uppercase text-2xl mr-4'
         :to="{ name: 'home' }"
-        exact-active-class="no-active"
-        >Music</router-link
-      >
-      <div class="flex flex-grow items-center">
+        exact-active-class='no-active'
+      >Music
+      </router-link>
+      <div class='flex flex-grow items-center'>
         <!-- Primary Navigation -->
-        <ul class="flex flex-row mt-1">
+        <ul class='flex flex-row mt-1'>
           <li>
-            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
+            <router-link class='px-2 text-white' :to="{ name: 'about' }">About</router-link>
           </li>
           <!-- Navigation Links -->
-          <li v-if="!userStore.userLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
-              >Login / Register</a
+          <li v-if='!userStore.userLoggedIn'>
+            <a class='px-2 text-white' href='#' @click.prevent='toggleAuthModal'
+            >Login / Register</a
             >
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
+              <router-link class='px-2 text-white' :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
+              <a class='px-2 text-white' href='#' @click.prevent='signOut'>Logout</a>
             </li>
           </template>
         </ul>
